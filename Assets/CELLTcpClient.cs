d0a6ffcc208961cf26653a81898135024e74953d
caijinchao <caijinchao@xd.com>
2021-05-13 19:08:41 +0800

    客户端提交

---
+++ Assets/CELLTcpClient.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+using AOT;
+
+public class CELLTcpClient : MonoBehaviour
+{
+    public CELLTcpClient()
+    {
+        _thisObj = IntPtr.Zero;
+        _cppClientObj = IntPtr.Zero;
+    }
+    private GCHandle _handle;
+    IntPtr _thisObj;
+    IntPtr _cppClientObj;
+
+    public delegate void OnNetMsgCallBack(IntPtr obj, IntPtr data, int len);
+    [MonoPInvokeCallback(typeof(OnNetMsgCallBack))]
+    public void DellOnNetMsgCallBack(IntPtr csObj, IntPtr data, int len)
+    {
+        Debug.Log(len);
+    }
+
+    [DllImport("CppNet100")]
+    public static extern IntPtr CELLClient_Create(IntPtr obj, OnNetMsgCallBack cb, int sendSize, int recvSize);
+
+    [DllImport("CppNet100")]
+    public static extern bool CELLClient_Connect(IntPtr cppClientObj, string ip, UInt16 port);
+
+    [DllImport("CppNet100")]
+    public static extern bool CELLClient_OnRun(IntPtr cppClientObj);
+
+    [DllImport("CppNet100")]
+    public static extern void CELLClient_Close(IntPtr cppClientObj);
+
+    [DllImport("CppNet100")]
+    public static extern int CELLClient_SendData(IntPtr cppClientObj, byte[] data, int len);
+
+
+    public void Creat()
+    {
+        _handle = GCHandle.Alloc(this);
+        _thisObj = GCHandle.ToIntPtr(_handle);
+
+        _cppClientObj = CELLClient_Create(_thisObj, DellOnNetMsgCallBack, 1024, 1024);
+    }
+
+    public bool Connect(string ip, UInt16 port)
+    {
+        if (_cppClientObj == IntPtr.Zero)
+        {
+            return false;
+        }
+
+        return CELLClient_Connect(_cppClientObj, ip, port);
+    }
+
+    public bool OnRun()
+    {
+        if(_cppClientObj == IntPtr.Zero)
+        {
+            return false;
+        }
+
+        return CELLClient_OnRun(_cppClientObj);
+    }
+
+    public void Close()
+    {
+        if (_cppClientObj == IntPtr.Zero)
+        {
+            return;
+        }
+
+        CELLClient_Close(_cppClientObj);
+    }
+
+    public int SendData(byte[] data, int len)
+    {
+        if (_cppClientObj == IntPtr.Zero)
+        {
+            return 0;
+        }
+
+        return CELLClient_SendData(_cppClientObj, data, len);
+    }
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+using AOT;
+
+public class NewBehaviourScript : CELLTcpClient
+{
+    private string _ip = "127.0.0.1";
+    private UInt16 _port = 4567;
+
+    [DllImport("CppNet100")]
+    public static extern int Add(int a, int b);
+
+    public delegate void CallBack1(string s);
+    [MonoPInvokeCallback(typeof(CallBack1))]
+    public void CallBackFun1(string s)
+    {
+        Debug.Log(s);
+    }
+
+    [DllImport("CppNet100")]
+    public static extern int TestCall1(string s, CallBack1 cb);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Creat();
+        Connect(_ip, _port);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnRun();
+    }
+
+    void OnDestroy()
+    {
+        Close();
+    }
+}
