540958dafc744c1be4a76480b76fd0b3f3099381
caihaha <1030878894@qq.com>
2021-05-16 16:24:29 +0800

    客户端读取数据

--- Assets/CELLReadStream.cs
+++ Assets/CELLReadStream.cs
@@ -6,25 +6,53 @@ using System.Runtime.InteropServices;
 
 public class CELLReadStream
 {
-    byte[] _buffer;
-    Int32 _lastPos;
+    IntPtr _cppStreamObj;
 
-    public byte[] Data => _buffer;
+    [DllImport("CppNet100")]
+    public static extern IntPtr CELLReadStream_Create(IntPtr data, int nSize);
+
+    [DllImport("CppNet100")]
+    public static extern sbyte CELLReadStream_ReadInt8(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern Int16 CELLReadStream_ReadInt16(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern Int32 CELLReadStream_ReadInt32(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern Int64 CELLReadStream_ReadInt64(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern byte CELLReadStream_ReadUInt8(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern UInt16 CELLReadStream_ReadUInt16(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern UInt32 CELLReadStream_ReadUInt32(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern UInt64 CELLReadStream_ReadUInt64(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern float CELLReadStream_ReadFloat(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern double CELLReadStream_ReadDouble(IntPtr cppObjStream);
+	
+	// [DllImport("CppNet100")]
+    // public static extern bool CELLReadStream_ReadString(IntPtr cppObjStream, IntPtr buffer, Int32 len);
+	
+	[DllImport("CppNet100")]
+    public static extern void CELLReadStream_Release(IntPtr cppObjStream);
+	
+	[DllImport("CppNet100")]
+    public static extern UInt32 CELLReadStream_OnlyReadUInt32(IntPtr cppObjStream);
 
     public CELLReadStream(IntPtr data, int len)
     {
-        byte[] _buffer = new byte[len];
-        Marshal.Copy(data, _buffer, 0, len);
-    }
-
-    private void Pop(Int32 n)
-    {
-        _lastPos += n;
-    }
-
-    private bool CanRead(Int32 n)
-    {
-        return _buffer.Length - _lastPos >= n;
+        CELLReadStream_Create(_cppStreamObj);
     }
 
     public UInt16 GetNetCmd()
@@ -34,139 +62,78 @@ public class CELLReadStream
 
     public sbyte ReadInt8()
     {
-        sbyte ret = 0;
-        if (CanRead(sizeof(sbyte)))
-        {
-            ret = (sbyte)_buffer[_lastPos];
-            Pop(sizeof(sbyte));
-        }
-
-        return ret;
+        return CELLReadStream_ReadInt8(_cppStreamObj);
     }
 
     public Int16 ReadInt16()
     {
-        Int16 ret = 0;
-        if(CanRead(sizeof(Int16)))
-        {
-            ret = BitConverter.ToInt16(_buffer, _lastPos);
-            Pop(sizeof(Int16));
-        }
-
-        return ret;
+        return CELLReadStream_ReadInt16(_cppStreamObj);
     }
 
     public Int32 ReadInt32()
     {
-        Int32 ret = 0;
-        if (CanRead(sizeof(Int32)))
-        {
-            ret = BitConverter.ToInt32(_buffer, _lastPos);
-            Pop(sizeof(Int32));
-        }
-
-        return ret;
+        return CELLReadStream_ReadInt32(_cppStreamObj);
     }
 
     public Int64 ReadInt64()
     {
-        Int64 ret = 0;
-        if (CanRead(sizeof(Int64)))
-        {
-            ret = BitConverter.ToInt64(_buffer, _lastPos);
-            Pop(sizeof(Int64));
-        }
-
-        return ret;
+        return CELLReadStream_ReadInt64(_cppStreamObj);
     }
 
     public byte ReadUInt8()
     {
-        byte ret = 0;
-        if (CanRead(sizeof(byte)))
-        {
-            ret = _buffer[_lastPos];
-            Pop(sizeof(byte));
-        }
-
-        return ret;
+        return CELLReadStream_ReadUInt8(_cppStreamObj);
     }
 
     public UInt16 ReadUInt16()
     {
-        UInt16 ret = 0;
-        if (CanRead(sizeof(UInt16)))
-        {
-            ret = BitConverter.ToUInt16(_buffer, _lastPos);
-            Pop(sizeof(UInt16));
-        }
-
-        return ret;
+        return CELLReadStream_ReadUInt16(_cppStreamObj);
     }
     public UInt32 ReadUInt32()
     {
-        UInt32 ret = 0;
-        if (CanRead(sizeof(UInt32)))
-        {
-            ret = BitConverter.ToUInt32(_buffer, _lastPos);
-            Pop(sizeof(UInt32));
-        }
-
-        return ret;
+        return CELLReadStream_ReadUInt32(_cppStreamObj);
     }
 
     public UInt64 ReadUInt64()
     {
-        UInt64 ret = 0;
-        if (CanRead(sizeof(UInt64)))
-        {
-            ret = BitConverter.ToUInt64(_buffer, _lastPos);
-            Pop(sizeof(UInt64));
-        }
-
-        return ret;
+        return CELLReadStream_ReadUInt64(_cppStreamObj);
     }
 
     public float ReadFloat()
     {
-        float ret = 0;
-        if (CanRead(sizeof(float)))
-        {
-            ret = BitConverter.ToSingle(_buffer, _lastPos);
-            Pop(sizeof(float));
-        }
-
-        return ret;
+        return CELLReadStream_ReadFloat(_cppStreamObj);
     }
 
     public double ReadDouble()
     {
-        double ret = 0;
-        if (CanRead(sizeof(double)))
-        {
-            ret = BitConverter.ToDouble(_buffer, _lastPos);
-            Pop(sizeof(double));
-        }
-
-        return ret;
+        return CELLReadStream_ReadDouble(_cppStreamObj);
     }
 
     public string ReadString()
     {
-        string ret = "";
-        Int32 len = 0;
-        if (CanRead(sizeof(Int32)))
-        {
-            len = BitConverter.ToInt32(_buffer, _lastPos);
-        }
-        if(CanRead(len) && len > 0)
-        {
-            Pop(sizeof(Int32));
-            ret = Encoding.UTF8.GetString(_buffer, _lastPos, len);
-            Pop(len);
-        }
-
-        return ret;
+		Int32 len = (Int32)ReadUInt32();
+		byte[] buffer = new byte[len];
+		for(int i = 0; i < len; ++i)
+		{
+			buffer[i] = ReadUInt8();
+		}
+		
+		return Encoding.UTF8.GetString(buffer, 0, len);
+		// string s = "";
+		// Int32 len = (Int32)OnlyReadUInt32();
+		// StringBuilder sb = new StringBuilder(len);
+		// CELLReadStream_ReadString(_cppStreamObj, sb, len);
+        // return sb.ToString();
+    }
+	
+	public string Release()
+    {
+        return CELLReadStream_Release(_cppStreamObj);
+    }
+	
+	public string OnlyReadUInt32()
+    {
+        return CELLReadStream_OnlyReadUInt32(_cppStreamObj)
     }
 
     public Int32[] ReadInts()
